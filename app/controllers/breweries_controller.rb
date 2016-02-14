d88acaeccf3ccee9d556d312fcc3327295f1e02f
uedi <uedi@users.noreply.github.com>
2016-02-14 20:13:40 +0200

    viikko 4: 12, 14-15

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,7 +1,7 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate, only: [:destroy]
-
+  before_action :ensure_that_signed_in, except: [:index, :show]
+  
   # GET /breweries
   # GET /breweries.json
   def index
@@ -72,11 +72,5 @@ class BreweriesController < ApplicationController
     def brewery_params
       params.require(:brewery).permit(:name, :year)
     end
-    
-    def authenticate
-      admin_accounts = { "admin" => "secret", "pekka" => "beer", "arto" => "foobar", "matti" => "ittam"}
-      authenticate_or_request_with_http_basic do |username, password|
-        admin_accounts[username] == password
-      end
-    end
+        
 end
